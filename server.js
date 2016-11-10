82a21975ea56bcb9e5bdadd272b013b811386d5b
Kevin Tran <kevin2005tran@gmail.com>
2016-11-10 11:13:22 -0800

    finished sprint 2, moving on to sprint 3

--- server.js
+++ server.js
@@ -20,34 +20,70 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
+// // get all books
+// app.get('/api/books', function (req, res) {
+//   // send all books as JSON response
+//   db.Book.find(function(err, books){
+//     if (err) { return console.log("index error: " + err); }
+//     res.json(books);
+//   });
+// });
+
+// // get one book
+// app.get('/api/books/:id', function (req, res) {
+//   // find one book by its id
+//   db.Book.findById(req.params.id, function(err, book){
+//     if (err) { return console.log("show error: " + err); }
+//     res.json(book);
+//   });
+// });
+
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  db.Book.find(function(err, books){
-    if (err) { return console.log("index error: " + err); }
-    res.json(books);
-  });
+  db.Book.find()
+    // populate fills in the author id with all the author data
+    .populate('author')
+    .exec(function(err, books){
+      if (err) { return console.log("index error: " + err); }
+      res.json(books);
+    });
 });
 
-// get one book
-app.get('/api/books/:id', function (req, res) {
-  // find one book by its id
-  db.Book.findById(req.params.id, function(err, book){
-    if (err) { return console.log("show error: " + err); }
-    res.json(book);
-  });
-});
+// // create new book
+// app.post('/api/books', function (req, res) {
+//   // create new book with form data (`req.body`)
+//   var newBook = new db.Book(req.body);
+//   // add newBook to database
+//   newBook.save(function(err, book){
+//     if (err) { return console.log("create error: " + err); }
+//     console.log("created ", book.title);
+//     res.json(book);
+//   });
+// });
 
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  var newBook = new db.Book(req.body);
+  var newBook = new db.Book({
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
+  });
+
+  // this code will only add an author to a book if the author already exists
+db.Author.findOne({name: req.body.author}, function(err, author){
+  newBook.author = author;
   // add newBook to database
   newBook.save(function(err, book){
-    if (err) { return console.log("create error: " + err); }
+    if (err) {
+      return console.log("create error: " + err);
+    }
     console.log("created ", book.title);
     res.json(book);
   });
+});
+
 });
 
 
